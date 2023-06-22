468258a3efe30bcd76ad96c3848f66f2b1d5ca64
dasuiyuan <adolf-me@163.com>
2023-06-22 16:15:09 +0800

    init analysis module

---
+++ st-analysis/src/main/java/com/chronomon/coretech/analysis/App.java
@@ -0,0 +1,13 @@
+package com.chronomon.coretech.analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello,here is chronomon!" );
+    }
+}
